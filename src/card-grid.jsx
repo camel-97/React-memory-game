b1efcb4ea6e61e0c0114e7c0a02b30983e642825
camel-97 <cfmcmillan@icloud.com>
2025-11-24 10:45:31 +0000

    added scoreboard utility, logic for score, high score and tracking doubled clicks to game over

--- src/card-grid.jsx
+++ src/card-grid.jsx
@@ -1,43 +1,74 @@
 import { useEffect, useState } from "react";
 import getRandomPokemon from "./pokemon-fetcher";
 import shuffle from "./array-shuffle";
+import Scoreboard from "./score-board";
 import './grid.css'
 
 
 export default function Grid() {
 
     const [cards, setCards] = useState([])
+    const [score, setScore] = useState(0)
+    const [highScore, setHighScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
     async function loadCards() {
         const newCards = []
         while (newCards.length < 12) {
             const imageUrl = await getRandomPokemon();
-            if (newCards.some(card => card.pokeUrl === imageUrl)){continue}
+            if (newCards.some(card => card.pokeUrl === imageUrl)) { continue }
             newCards.push({ id: newCards.length, pokeUrl: imageUrl, clicked: false })
         }
         setCards(newCards);
     }
 
-    function shuffleHandler(){
-        const shuffled = [...cards]
-        const shufArray = shuffle(shuffled)
-        setCards(shufArray)
-    }
+    function handleClick(cardId) {
+            const clickedCard = cards.find(c => c.id === cardId)
+            if (clickedCard.clicked) {
+                setGameOver(true);
+                setScore(prevScore => {
+                    setHighScore(hs => Math.max(hs, prevScore))
+                    return 0;
+                });
+                return;
+            } 
+            const updatedCards = cards.map(c => c.id === cardId ? { ...c, clicked: true } : c);
+
+            const shuffled = shuffle([...updatedCards]);
+            setCards(shuffled);
+            setScore(prev => prev + 1);
+        }
 
     useEffect(() => { loadCards() }, [])
 
     return (
-        <div className="grid">
-            {cards.map(card => (
-                    <div className="card" key={card.id}>
-                        <div className="poke-img">
-                            <img src={card.pokeUrl} onClick={
-                                shuffleHandler
-                            }/>
+        <>
+            <Scoreboard
+                score={score}
+                highScore={highScore}
+            ></Scoreboard>
+            {gameOver ? (
+                <div className="restart-div">
+                    <button onClick={() => {
+                        setGameOver(false);
+                        loadCards()
+                    }}>Ready to Play Again?</button>
+                </div>
+            ) : (
+                <div className="grid">
+                    {cards.map(card => (
+                        <div className="card" key={card.id}>
+                            <div className="poke-img">
+                                <img src={card.pokeUrl} onClick={
+                                    () => handleClick(card.id)
+                                }
+                                />
+                            </div>
                         </div>
-                    </div>
-                )
-        )}
-        </div>
+                    )
+                    )}
+                </div>
+            )}
+        </>
     )
 }
