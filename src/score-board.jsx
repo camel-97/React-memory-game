cbe30c077beebf12cd5bd98ae86d9c773306e9ca
camel-97 <cfmcmillan@icloud.com>
2025-11-25 19:34:00 +0000

    initialize scoreboard bar with score and highscore props

--- src/score-board.jsx
+++ src/score-board.jsx
@@ -1,9 +1,11 @@
+import './score-board.css'
+
 export default function Scoreboard({score, highScore}) {
     return (
-        <div className="score-board">
-            <div className="score">Score: {score}</div>
-            <div className="high-score">High Score: {highScore}</div>
-        </div>
+            <div className="score-board">
+                <div className="score">Score: {score}</div>
+                <div className="high-score">High Score: {highScore}</div>
+            </div>
     )
 
 }
