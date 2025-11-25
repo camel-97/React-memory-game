4f4f6dba5fff6c5048d0302e6451182764fff8cd
camel-97 <cfmcmillan@icloud.com>
2025-11-25 19:10:00 +0000

    late commit, fully functional for 12 cards, hard mode, tested animations but opted for simplisctic visual

--- src/card-grid.jsx
+++ src/card-grid.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import getRandomPokemon from "./pokemon-fetcher";
 import shuffle from "./array-shuffle";
+import { v4 as uuidv4 } from 'uuid';
 import Scoreboard from "./score-board";
+import './index.css'
 import './grid.css'
 
 
@@ -17,27 +19,34 @@ export default function Grid() {
         while (newCards.length < 12) {
             const imageUrl = await getRandomPokemon();
             if (newCards.some(card => card.pokeUrl === imageUrl)) { continue }
-            newCards.push({ id: newCards.length, pokeUrl: imageUrl, clicked: false })
+            newCards.push({ id: uuidv4(), pokeUrl: imageUrl, clicked: false })
         }
         setCards(newCards);
     }
 
     function handleClick(cardId) {
-            const clickedCard = cards.find(c => c.id === cardId)
-            if (clickedCard.clicked) {
-                setGameOver(true);
-                setScore(prevScore => {
-                    setHighScore(hs => Math.max(hs, prevScore))
-                    return 0;
-                });
-                return;
-            } 
-            const updatedCards = cards.map(c => c.id === cardId ? { ...c, clicked: true } : c);
-
-            const shuffled = shuffle([...updatedCards]);
-            setCards(shuffled);
-            setScore(prev => prev + 1);
+        const clickedCard = cards.find(c => c.id === cardId)
+        if (clickedCard.clicked) {
+            setGameOver(true);
+            setScore(prevScore => {
+                setHighScore(hs => Math.max(hs, prevScore))
+                return 0;
+            });
+            return;
         }
+
+            setTimeout(() => {
+                const updatedCards = cards.map(c =>
+                    c.id === cardId ? { ...c, clicked: true } : c);
+
+                const shuffled = shuffle([...updatedCards]);
+                setCards(shuffled);
+                setScore(prev => prev + 1);
+
+                setIsShuffling(false)
+
+            }, 300)
+    }
 
     useEffect(() => { loadCards() }, [])
 
@@ -47,6 +56,7 @@ export default function Grid() {
                 score={score}
                 highScore={highScore}
             ></Scoreboard>
+            <div className="game-container">
             {gameOver ? (
                 <div className="restart-div">
                     <button onClick={() => {
@@ -55,20 +65,22 @@ export default function Grid() {
                     }}>Ready to Play Again?</button>
                 </div>
             ) : (
-                <div className="grid">
-                    {cards.map(card => (
-                        <div className="card" key={card.id}>
-                            <div className="poke-img">
-                                <img src={card.pokeUrl} onClick={
-                                    () => handleClick(card.id)
-                                }
-                                />
+                    <div className="grid">
+                        {cards.map(card => (
+                            <div
+                                className="card"
+                                key={card.id}
+                            >
+                                <div className="poke-img">
+                                    <img src={card.pokeUrl} onClick={() => handleClick(card.id)}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    )
-                    )}
-                </div>
+                        )
+                        )}
+                    </div>
             )}
+            </div>
         </>
     )
 }
