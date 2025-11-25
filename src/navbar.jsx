4c22f0e6fe77da6941a1bec87fa63d465465967b
camel-97 <cfmcmillan@icloud.com>
2025-11-25 20:43:40 +0000

    set difficulty on each button click 6, 9, 12 respectively

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,13 +1,14 @@
+
 import './nav.css'
 
-export default function Nav() {
+export default function Nav({setDifficulty}) {
     return (
         <div className="nav-bar">
             <img src="https://fontmeme.com/permalink/251125/e1e5498b622b1fb5994a68ba0a44a64f.png" alt="pokemon-font"/>
             <div className='difficulty-div'>
-                <button>Easy</button>
-                <button>Medium</button>
-                <button>Hard</button>
+                <button onClick={() => setDifficulty(6)}>Easy</button>
+                <button onClick={() => setDifficulty(9)}>Medium</button>
+                <button onClick={() => setDifficulty(12)}>Hard</button>
             </div>
         </div>
     )
