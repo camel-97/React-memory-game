2ab79eca2fc6b32b3911e2d8e025773295c88cc1
camel-97 <cfmcmillan@icloud.com>
2025-11-27 13:02:59 +0000

    difficulty props handled correctly

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,7 +4,9 @@ import './nav.css'
 export default function Nav({setDifficulty}) {
     return (
         <div className="nav-bar">
-            <img src="https://fontmeme.com/permalink/251125/e1e5498b622b1fb5994a68ba0a44a64f.png" alt="pokemon-font"/>
+            
+                <img src="https://fontmeme.com/permalink/251125/e1e5498b622b1fb5994a68ba0a44a64f.png" alt="pokemon-font"/>
+            
             <div className='difficulty-div'>
                 <button onClick={() => setDifficulty(6)}>Easy</button>
                 <button onClick={() => setDifficulty(9)}>Medium</button>
